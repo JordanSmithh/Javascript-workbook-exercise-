4b0fd5a0247b05023b509eb19f87a4e8d3d5a234
Jordan Smith <Jordansmithformally@gmail.com>
2024-10-28 13:30:48 -0400

    answers to exercise 1

---
+++ script.js
@@ -0,0 +1,23 @@
+var customerId;
+customerId ="98987654"
+
+var firstName;
+firstName ="Jason";
+
+var middleName;
+middleName ="Tracey";
+
+var lastName;
+lastName ="Graham";
+
+var gender;
+gender ="Male";
+
+var dateOfBirth;
+dateOfBirth ="July 17 2001";
+
+var driverLicenseNumber;
+driverLicenseNumber ="9087643683658";
+
+var autoPolicyNumber;
+autoPolicyNumber ="7367996319";
